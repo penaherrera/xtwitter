f1e7f6b92a2d5335860895ba98fbbfa5e8bff4e0
penaherrera <cpena@ioogo.com>
2023-09-20 13:28:20 -0600

    Added the join table between tweet and hashtag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_154932) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_172608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_154932) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_bookmarks_on_author_id"
     t.index ["tweet_id"], name: "index_bookmarks_on_tweet_id"
+  end
+
+  create_table "create_join_table_hastags_tweets", force: :cascade do |t|
+    t.string "hashtags"
+    t.string "tweets"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "followers", force: :cascade do |t|
