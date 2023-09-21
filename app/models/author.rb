8a5eee814c2e4581f2f2ce7ff44997561fd67ae6
penaherrera <cpena@ioogo.com>
2023-09-21 09:08:13 -0600

    Fixing relations between Followers and Author (user)

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,6 +5,10 @@ class Author < ApplicationRecord
     has_many :quotes
     has_many :replies
     has_many :retweets
-    has_many :followers
-    has_many :following
+
+    has_many :follower_relationships, class_name: "Follower", foreign_key: "following_username"
+    has_many :followers, through: :follower_relationships, source: :follower
+
+    has_many :following_relationships, class_name: "Follower", foreign_key: "follower_username"
+    has_many :following, through: :following_relationships, source: :following
   end
--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,4 +1,4 @@
 class Follower < ApplicationRecord
-  belongs_to :follower
-  belongs_to :following
+  belongs_to :follower, class_name: "Author", foreign_key: "follower_username", primary_key: "username"
+  belongs_to :following, class_name: "Author", foreign_key: "following_username", primary_key: "username"
 end
