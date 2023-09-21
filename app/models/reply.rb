08a44c57296a23158f4c52fee4bb76f0df973090
penaherrera <cpena@ioogo.com>
2023-09-21 15:11:29 -0600

    Deleting validation_association to avoid loop bug

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -4,5 +4,4 @@ class Reply < ApplicationRecord
 
   #validations
   validates :content, length:{ maximum: 255 }, presence: true
-  validates_associated :tweet, :author
 end
--- app/models/retweet.rb
+++ app/models/retweet.rb
@@ -2,9 +2,7 @@ class Retweet < ApplicationRecord
   belongs_to :tweet
   belongs_to :author
 
-  #validations
 
-  validates :body, length: { maximum: 255 }, allow_nil: true
 
-  validates_associated :tweet, :author
+
 end
