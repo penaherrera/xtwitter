47bd0ff1885bb58d0f3e4d866292f518f8b6ccbc
penaherrera <cpena@ioogo.com>
2023-09-21 09:35:51 -0600

    Adding validation for reply model

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,7 @@
 class Reply < ApplicationRecord
   belongs_to :tweet
   belongs_to :author
+
+  validates :content, length:{ maximum: 255 }, presence: true
+  validates_associated :tweet, :author
 end
