46820c6db30b4978b031cafa9207c8803f8beb07
penaherrera <cpena@ioogo.com>
2023-09-22 13:22:07 -0600

    Adding Like Method

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,8 +3,21 @@ class Like < ApplicationRecord
   belongs_to :author
 
   #validations
-
-
-
   validates_uniqueness_of :author_id, scope: :tweet_id
+
+  #Methods
+  #Like a tweet: Create a method that encapsulates the like logic accepting a user an a text body as parameter
+
+  def self.create_like(author, tweet)
+    like = Like.new(
+      author_id: author.id,
+      tweet_id: tweet.id,
+    )
+
+    if like.save
+      return "Like created successfully."
+    else
+      return "Like creation failed."
+    end
+  end
 end
