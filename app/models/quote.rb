004561c73e6e9743ba9f1fe4de50b81c122c991f
penaherrera <cpena@ioogo.com>
2023-09-22 11:52:43 -0600

    Create Quote method

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -7,4 +7,20 @@ class Quote < ApplicationRecord
 
   #Scopes
   scope :quotes_from_author, ->(author_id) { where(author_id: author_id) }
+
+  #Method
+  #QuoteTweet: Create a method that encapsulates the retweet logic accepting a user an a text body as parameter
+  def self.create_quote(content, author, tweet)
+    quote = Quote.new(
+      content: content,
+      author_id: author.id,
+      tweet_id: tweet.id,
+    )
+
+    if quote.save
+      return "Quote created successfully."
+    else
+      return "Quote creation failed."
+    end
+  end
 end
