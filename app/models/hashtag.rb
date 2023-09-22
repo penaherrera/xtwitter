63c49f489483abfeca9a3bc28d141c7f2781e506
penaherrera <cpena@ioogo.com>
2023-09-22 14:29:25 -0600

    Added Hasthag Method and Tweet Method

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,41 @@
 class Hashtag < ApplicationRecord
-    #validations
+    # validations
     validates :name, presence: true
-end
+  
+    # Method to create a hashtag
+    def self.create_hashtag(tweet)
+      tweet_body = tweet.body
+      tweet_body_array = tweet_body.split
+  
+      word_counts = {}
+  
+      hashtag_word = nil 
+  
+      tweet_body_array.each do |word|
+        word = word.downcase
+  
+        if word_counts.key?(word)
+          hashtag_word = "##{word}" 
+          break 
+        else
+          word_counts[word] = 1
+        end
+      end
+  
+
+      if hashtag_word
+        new_hashtag = Hashtag.new(
+          name: hashtag_word
+        )
+  
+        if new_hashtag.save
+          return "Hashtag created successfully: #{hashtag_word}"
+        else
+          return "Hashtag creation failed."
+        end
+      else
+        return "No repeated words found in the tweet."
+      end
+    end
+  end
+  
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,10 +6,23 @@ class Tweet < ApplicationRecord
   #validations
   validates :body, length:{ maximum: 255 }, presence: true
 
-  validates_associated :author
+
 
   #scopes
   scope :tweets_from_author, ->(author_id) { where(author_id: author_id) }
-
+  #method
+  def self.create_new_tweet(body, author)
+    new_tweet = Tweet.new( # Assuming you meant "Tweet" instead of "Retweet"
+      body: body,
+      author_id: author.id,
+    )
+  
+    if new_tweet.save # Changed "retweet" to "new_tweet"
+      return "A tweet was created successfully."
+    else
+      return "Tweet creation failed."
+    end
+  end
+  
 
 end
