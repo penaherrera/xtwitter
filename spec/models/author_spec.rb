7caa78ad1053bc981c0d879e4e3e9591b70931bd
penaherrera <cpena@ioogo.com>
2023-09-25 11:45:44 -0600

    adding test to run RSpec example

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can run tests" do
+    expect(false).to be(true)
+  end
 end
