41eb1ec4fb16d8b8eba32fa683204cb93a1094ae
penaherrera <cpena@ioogo.com>
2023-09-28 15:52:52 -0600

    setting up gem and dummy info for test

---
+++ spec/requests/authors_request_spec.rb
@@ -0,0 +1,28 @@
+# spec/integration/authors_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe 'Authors API', type: :request do
+  describe 'GET /authors/:id' do
+    let(:author) do
+      Author.create(
+        username: 'john_doe',
+        name: 'John',
+        lastname: 'Doe',
+        email: 'john@example.com',
+        password: 'password123',
+        created_at: Time.now,
+        updated_at: Time.now
+      )
+    end
+
+    it 'returns the correct author name in the JSON response' do
+      get "/authors/#{author.id}"
+
+      expect(response).to have_http_status(200) # Ensure the response is successful
+
+      author_response = JSON.parse(response.body)
+      expect(author_response['name']).to eq(author.name)
+    end
+  end
+end
