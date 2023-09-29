4fab2ea1c6954f70855821b8b98b0c5f47570941
Carlos Pena <carlos.cpa26@gmail.com>
2023-09-28 22:33:37 -0600

    Adding quote crate request spec

---
+++ spec/requests/quote_create_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+require 'json-schema'
+
+RSpec.describe "Quote Creation", type: :request do
+  describe "POST /tweets/:id/quote" do
+    it "routes to the Quotes controller" do
+      expect(post: "/tweets/1/quote").to route_to(controller: "quotes", action: "create", id: "1")
+    end
+
+    it "creates a quote successfully with valid data" do
+      author = create(:author)
+      tweet = create(:tweet, author: author)
+
+      valid_quote_params = { quote: { content: "This is a test quote" } }
+
+      valid_quote_json = valid_quote_params.to_json
+
+      post "/tweets/#{tweet.id}/quote", params: valid_quote_json, headers: { 'Content-Type': 'application/json' }
+
+      expect(response).to have_http_status(:success)
+
+    end
+
+    it "returns a validation error with invalid data" do
+      author = create(:author)
+      tweet = create(:tweet, author: author)
+
+      invalid_quote_params = { quote: {} }
+
+      invalid_quote_json = invalid_quote_params.to_json
+
+      post "/tweets/#{tweet.id}/quote", params: invalid_quote_json, headers: { 'Content-Type': 'application/json' }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      expect(response.body).to match_json_schema('quote_create.json')
+
+    end
+  end
+end
